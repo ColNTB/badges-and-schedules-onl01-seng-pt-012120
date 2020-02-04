bfb001471c1b996bbe166a5b559cd68dbe26be31
Nathan Bohn <nathantbohn@gmail.com>
2020-02-04 19:29:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,29 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end 
+
 def batch_badge_creator(array)
+  counter = 0; 
   array.each do |name|
-    batch =
+    batch = []
+    batch[counter] = badge_maker(name)
+    counter+=1 
+  end 
+  return batch 
+end 
+
+def assign_rooms(array)
+  counter = 0 
+  room = 1 
+  array.each do |speaker|
+    array[counter] = "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    room+=1 
+    counter+=1 
+  end 
+end 
+
+def printer
+  
+    
+  
+
